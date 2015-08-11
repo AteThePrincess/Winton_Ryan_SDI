9d7ac3fad31dddff5ad859ba3477fc7169a6da90
AteThePrincess <rdwinton@fullsail.edu>
2015-08-11 11:37:20 -0400

    Fleshing it out a bit

--- Winton_Ryan_Expressions_Assignment/js/script.js
+++ Winton_Ryan_Expressions_Assignment/js/script.js
@@ -1,18 +1,26 @@
 /*
-
-
+Ryan Winton
+SDI Secion 01
+Instructor Lee Lewis
+Expressions Assignment
 */
 alert("JavaScript works!");
 
 // Create Wire Length variable
 var wireLength;
 // Create Electrical Resistance variable
-var elecRes;
+var elecResistance;
 // Create Cross-sectional Area variable
 var csArea;
+
+
 // Request Wire Length and store it in variable wireLength
 prompt("What is the length of the wire?");
-// Request Electrical Resistance and store it in variable elecRes
+// Request Electrical Resistance and store it in variable elecResistance
 prompt("What is the Electrical Resistance of your wire?");
 // Request Cross-sectional Area and store it in variable csArea
 prompt("What is the Cross-sectional Area of the wire?");
+
+
+var elecResistivity = (elecResistance * csArea) / wireLength;
+command.log("The Electrical Resistivity is " + elecResistivity)
