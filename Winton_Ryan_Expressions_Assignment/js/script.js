ee2f8eacf99e98a075a0e0d8d0b79be51fab10ea
AteThePrincess <rdwinton@fullsail.edu>
2015-08-11 11:29:38 -0400

    Basic laying out of the JS

--- Winton_Ryan_Expressions_Assignment/js/script.js
+++ Winton_Ryan_Expressions_Assignment/js/script.js
@@ -1,1 +1,18 @@
-alert("This is in the js file");
+/*
+
+
+*/
+alert("JavaScript works!");
+
+// Create Wire Length variable
+var wireLength;
+// Create Electrical Resistance variable
+var elecRes;
+// Create Cross-sectional Area variable
+var csArea;
+// Request Wire Length and store it in variable wireLength
+prompt("What is the length of the wire?");
+// Request Electrical Resistance and store it in variable elecRes
+prompt("What is the Electrical Resistance of your wire?");
+// Request Cross-sectional Area and store it in variable csArea
+prompt("What is the Cross-sectional Area of the wire?");
