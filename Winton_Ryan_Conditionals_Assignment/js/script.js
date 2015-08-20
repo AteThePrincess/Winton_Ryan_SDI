a4c6f963c3687a825422caa7d91d99ca1d90abd3
AteThePrincess <rdwinton@fullsail.edu>
2015-08-20 14:05:31 -0700

    Now calculates KM to MI if need be.

--- Winton_Ryan_Conditionals_Assignment/js/script.js
+++ Winton_Ryan_Conditionals_Assignment/js/script.js
@@ -10,7 +10,14 @@ var carOwned;
 var unitOfMeasureMiles;
 
 var distance = prompt("How far is the store from here?"); //Find out how far the store is.
-var unitOfMeasureMiles = confirm("Our calculator needs distance in miles.\nIf your distance is in KM press cancel.\nIf your distance is in MI press OK.");
+var unitOfMeasureMiles = confirm("Our calculator needs distance in miles.\nIf your distance is in KM press cancel.\nIf your distance is in MI press OK."); //Ensure unit of measure is Miles
+if(unitOfMeasureMiles === false){
+    distance*=0.621371;
+    console.log("The store is " + distance + " miles away.")
+}
+else {
+
+}
 
 var carOwned = confirm("Do you own a car? Press OK for yes, cancel for no.");
 
@@ -23,7 +30,7 @@ if(Number(distance) < 1.5){
     console.log("User should walk to the store.")
 }
 
-else if(carOwned === true && Number(distance) > 6.0){
+else if(carOwned === true && Number(distance) > 6.5){
     console.log("User should drive to the store.")
 }
 
