620d248ac99f8ba4f3b70506f30cdb64b35e7140
AteThePrincess <rdwinton@fullsail.edu>
2015-08-20 15:08:37 -0700

    FIN!

--- Winton_Ryan_Conditionals_Assignment/js/script.js
+++ Winton_Ryan_Conditionals_Assignment/js/script.js
@@ -11,6 +11,8 @@ var unitOfMeasureMiles;
 
 var distance = prompt("How far is the store from here?"); //Find out how far the store is.
 
+(distance <= 0) ? console.log("The store is " + distance + "miles from here.") : console.log("Please insert a real distance in miles"); //Ternary Operator for making sure the distance is a properly usable number.
+
 var carOwned = confirm("Do you own a car? Press OK for yes, cancel for no."); // Determine if user owns a motor vehicle.
 
 var unitOfMeasureMiles = confirm("Our calculator needs distance in miles.\nIf your distance is in KM press cancel.\nIf your distance is in MI press OK."); //Ensure unit of measure is Miles
@@ -19,13 +21,7 @@ if(unitOfMeasureMiles === false){
     console.log("The store is " + distance + " miles away.")
 } //Turns distance in KM to MI
 else {
-
 } // Leaves distance in MI if it already is.
-
-
-(distance <= 0) ? console.log("The store is " + distance + "miles from here.") : console.log("Please insert a real distance in miles"); //Ternary Operator for making sure the distance is a properly usable number.
-
-
 
 //If the distance is less than 1.5 miles they will walk. If the distance is over 6.5 miles AND the user owns a car, the car will be taken. Otherwise they will ride their bicycle.
 if(Number(distance) < 1.5){
