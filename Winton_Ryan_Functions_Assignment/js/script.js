31a927d2becc7f73d2e6504b0b4f961e4e66a617
AteThePrincess <rdwinton@fullsail.edu>
2015-08-27 12:38:06 -0700

    Random generation working properly

--- Winton_Ryan_Functions_Assignment/js/script.js
+++ Winton_Ryan_Functions_Assignment/js/script.js
@@ -8,19 +8,17 @@ console.log("JS is working");
 
 var lottoType = confirm("Are you looking for Florida Lottery or Powerball numbers?/nPress OK for Florida Lottery numbers./nPress Cancel for Powerball numbers."); //Figuring out which lottery they're looking for.
 
-if(lottoType === true){
-    var drawMax = 53;
-    var drawMin = 1;
-}
-else {
-    var drawMax = 59;
-    var drawMin = 1;
-}
 
-pickNumbers(drawMax, drawMin);
-
-function pickNumbers() {
+function pickNumbers(drawMax, drawMin) {
 
     var drawnNumber = Math.ceil(Math.random() * drawMax - drawMin) + drawMin;
     console.log(drawnNumber);
 }
+
+if(lottoType === true){
+    pickNumbers(53, 1);
+}
+else {
+    pickNumbers(59, 1);
+}
+
