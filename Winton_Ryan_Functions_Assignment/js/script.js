d291454a6804ea13a8037bdc43d4577f9166d3cd
AteThePrincess <rdwinton@fullsail.edu>
2015-08-27 13:02:44 -0700

    Got return working properly now.

--- Winton_Ryan_Functions_Assignment/js/script.js
+++ Winton_Ryan_Functions_Assignment/js/script.js
@@ -8,17 +8,17 @@ console.log("JS is working");
 
 var lottoType = confirm("Are you looking for Florida Lottery or Powerball numbers?/nPress OK for Florida Lottery numbers./nPress Cancel for Powerball numbers."); //Figuring out which lottery they're looking for.
 
-
 function pickNumbers(drawMax, drawMin) {
 
     var drawnNumber = Math.ceil(Math.random() * drawMax - drawMin) + drawMin;
-    console.log(drawnNumber);
+    return drawnNumber;
 }
 
 if(lottoType === true){
-    pickNumbers(53, 1);
+    var randomNumber = pickNumbers(53, 1);
 }
 else {
-    pickNumbers(59, 1);
+    var randomNumber = pickNumbers(59, 1);
 }
 
+console.log(randomNumber);
