674357eeafe4622e9cb4476a13634352935e1f9b
AteThePrincess <rdwinton@fullsail.edu>
2015-08-27 12:02:31 -0700

    Just starting!

---
+++ Winton_Ryan_Functions_Assignment/js/script.js
@@ -0,0 +1,16 @@
+/*
+Ryan Winton
+SDI Secion 01
+Instructor Lee Lewis
+Functions Assignment
+*/
+console.log("JS is working");
+
+prompt("Are you looking for Florida Lottery or Powerball numbers?/nPress OK for Florida Lottery numbers./nPress Cancel for Powerball numbers."); //Figuring out which lottery they're looking for.
+
+function pickNumbers() {
+
+    return math.ceil((math.random() * pBallMax - allMin) + allMin);
+
+}
+
