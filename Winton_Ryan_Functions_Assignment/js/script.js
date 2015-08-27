7af2b41eb391b51cff59b61252153074a7bbe138
AteThePrincess <rdwinton@fullsail.edu>
2015-08-27 14:20:52 -0700

    FINAL.

--- Winton_Ryan_Functions_Assignment/js/script.js
+++ Winton_Ryan_Functions_Assignment/js/script.js
@@ -4,8 +4,6 @@ SDI Secion 01
 Instructor Lee Lewis
 Functions Assignment
 */
-console.log("JS is working");
-
 var lottoType = confirm("Are you looking for Florida Lottery or Powerball numbers?\nPress OK for Florida Lottery numbers.\nPress Cancel for Powerball numbers."); //Figuring out which lottery they're looking for.
 var ballVar = 0; // Sets up our while statement for later
 
