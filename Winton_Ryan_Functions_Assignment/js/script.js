4f82a1db26acc38c2a81771d37d9663d717f7215
AteThePrincess <rdwinton@fullsail.edu>
2015-08-27 13:21:40 -0700

    Fixed backslashes and got the whole thing working properly, just need to eliminate doubles

--- Winton_Ryan_Functions_Assignment/js/script.js
+++ Winton_Ryan_Functions_Assignment/js/script.js
@@ -6,7 +6,7 @@ Functions Assignment
 */
 console.log("JS is working");
 
-var lottoType = confirm("Are you looking for Florida Lottery or Powerball numbers?/nPress OK for Florida Lottery numbers./nPress Cancel for Powerball numbers."); //Figuring out which lottery they're looking for.
+var lottoType = confirm("Are you looking for Florida Lottery or Powerball numbers?\nPress OK for Florida Lottery numbers.\nPress Cancel for Powerball numbers."); //Figuring out which lottery they're looking for.
 var ballVar = 0;
 
 function pickNumbers(drawMax, drawMin) {
@@ -16,15 +16,23 @@ function pickNumbers(drawMax, drawMin) {
 }
 
 if(lottoType === true) {
+    var lotteryArray = new Array;
     while(ballVar < 6) {
-        var lotteryArray = new Array;
         var randomNumber = pickNumbers(53, 1);
         lotteryArray.push(randomNumber);
         ballVar++;
     }
+    console.log("Your winning numbers are " + lotteryArray[0] + ", " + lotteryArray[1] + ", " + lotteryArray[2] + ", " + lotteryArray[3] + ", " + lotteryArray[4] + ", " + lotteryArray[5] + ".");
 }
 else {
-    var randomNumber = pickNumbers(59, 1);
+    var lotteryArray = new Array;
+    while(ballVar < 5) {
+        var randomNumber = pickNumbers(59, 1);
+        lotteryArray.push(randomNumber);
+        ballVar++;
+    }
+    var PB = pickNumbers(35, 1);
+    console.log("Your winning numbers are " + lotteryArray[0] + ", " + lotteryArray[1] + ", " + lotteryArray[2] + ", " + lotteryArray[3] + ", " + lotteryArray[4] + ", and your PowerBall number is: " + PB + ".");
+
 }
 
-console.log("Your winning numbers are " + lotteryArray[0] + ", " + lotteryArray[1] + ", " + lotteryArray[2] + ", " + lotteryArray[3] + ", " + lotteryArray[4] + ", " + lotteryArray[5] + ".");
