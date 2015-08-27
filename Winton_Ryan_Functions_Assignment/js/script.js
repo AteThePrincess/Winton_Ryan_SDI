39ccb38bcacf5fa8077f6cc5cf90de16dc84dbda
AteThePrincess <rdwinton@fullsail.edu>
2015-08-27 12:23:32 -0700

    Drawing one random number, albeit not in the right manner... Hmm.

--- Winton_Ryan_Functions_Assignment/js/script.js
+++ Winton_Ryan_Functions_Assignment/js/script.js
@@ -6,11 +6,21 @@ Functions Assignment
 */
 console.log("JS is working");
 
-prompt("Are you looking for Florida Lottery or Powerball numbers?/nPress OK for Florida Lottery numbers./nPress Cancel for Powerball numbers."); //Figuring out which lottery they're looking for.
+var lottoType = confirm("Are you looking for Florida Lottery or Powerball numbers?/nPress OK for Florida Lottery numbers./nPress Cancel for Powerball numbers."); //Figuring out which lottery they're looking for.
+
+if(lottoType === true){
+    var drawMax = 53;
+    var drawMin = 1;
+}
+else {
+    var drawMax = 59;
+    var drawMin = 1;
+}
+
+pickNumbers(drawMax, drawMin);
 
 function pickNumbers() {
 
-    return math.ceil((math.random() * pBallMax - allMin) + allMin);
-
+    var drawnNumber = Math.ceil(Math.random() * drawMax - drawMin) + drawMin;
+    console.log(drawnNumber);
 }
-
